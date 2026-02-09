0e22198eb02c72b17b2507878d1cf4928d38970a
cpa03 <245960241+cpa03@users.noreply.github.com>
2026-02-09 08:10:10 +0000

    fix: resolve CONTEXT_CONFIG import and merge conflicts from develop

--- packages/core/src/services/context/constants.ts
+++ packages/core/src/services/context/constants.ts
@@ -6,6 +6,7 @@
 import type { ContextMode } from './types';
 import { CONTEXT_CONFIG } from '../../config/core-config';
 import { CONTEXT_VERSIONS } from '../../constants/versions';
+import { CONTEXT_CONFIG } from '../../config/core-config';
 
 // 存储键
 export const CONTEXT_STORE_KEY = CONTEXT_CONFIG.storeKey;
